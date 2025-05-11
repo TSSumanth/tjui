fedc71d9ebe3b929dc772a72b2c1f5e7421ded83
Sumanth <sumanth.talamanchi@passportinc.com>
2025-05-11 17:48:37 +0530

    improvemnets

--- src/services/marketanalysis.js
+++ src/services/marketanalysis.js
@@ -1,8 +1,16 @@
 import axios from "axios";
 import { API_URLS } from '../config/api';
 
-export const getAllMarketAnalysis = async ({ page = 1, limit = 10 } = {}) => {
-    const response = await axios.get(API_URLS.MARKET_ANALYSIS + `?page=${page}&limit=${limit}`);
+export const getAllMarketAnalysis = async ({ page = 1, limit = 10, startDate, endDate, searchText, premarketexpectation, marketmovement } = {}) => {
+    const params = new URLSearchParams();
+    params.append('page', page);
+    params.append('limit', limit);
+    if (startDate) params.append('startDate', startDate);
+    if (endDate) params.append('endDate', endDate);
+    if (searchText) params.append('searchText', searchText);
+    if (premarketexpectation) params.append('premarketexpectation', premarketexpectation);
+    if (marketmovement) params.append('marketmovement', marketmovement);
+    const response = await axios.get(API_URLS.MARKET_ANALYSIS + `?${params.toString()}`);
     return response.data;
 };
 
